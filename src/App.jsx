0d70cb32350a259d5bbdf424d6a50edcb84d5367
Ajayaditya-06 <ajayaditya.dev@proton.me>
2024-03-09 17:40:42 +0530

    background color change on scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useScrollYPosition } from "react-use-scroll-position";
 import Issue1 from "./issues/issues1";
 import Issue2 from "./issues/issues2";
 import Issue3 from "./issues/issues3";
@@ -6,10 +7,68 @@ import Issue5 from "./issues/issues5";
 import Issue6 from "./issues/issues6";
 import Issue7 from "./issues/issues7";
 import Overlay from "./issues/overlay";
+import { useEffect } from "react";
 
 export default function App() {
+  const scrolly = useScrollYPosition();
+  const minScrollHandler = () => {
+    if (scrolly < window.innerHeight) {
+      document.getElementById("issue").style.backgroundColor = "#FF608C";
+      document.body.style.color = "#000";
+    } else if (scrolly < 2 * window.innerHeight) {
+      document.getElementById("issue").style.backgroundColor = "#fff";
+      document.body.style.color = "#000";
+    } else if (scrolly < 3 * window.innerHeight) {
+      document.getElementById("issue").style.backgroundColor = "#00C1B5";
+      document.body.style.color = "#000";
+    } else if (scrolly < 4 * window.innerHeight) {
+      document.getElementById("issue").style.backgroundColor = "#FF6519";
+      document.body.style.color = "#000";
+    } else if (scrolly < 5 * window.innerHeight) {
+      document.getElementById("issue").style.backgroundColor = "#FFBE00";
+      document.body.style.color = "#000";
+    } else if (scrolly < 6 * window.innerHeight) {
+      document.getElementById("issue").style.backgroundColor = "#1D3FBB";
+      document.body.style.color = "#000";
+    } else if (scrolly < 7 * window.innerHeight) {
+      document.getElementById("issue").style.backgroundColor = "#E30512";
+      document.body.style.color = "#000";
+    }
+  };
+  const scrollHandler = (e) => {
+    console.log(e.target.scrollTop);
+    if (e.target.scrollTop < window.innerHeight) {
+      document.getElementById("issue").style.backgroundColor = "#FF608C";
+      document.body.style.color = "#000";
+    } else if (e.target.scrollTop < 2 * window.innerHeight) {
+      document.getElementById("issue").style.backgroundColor = "#fff";
+      document.body.style.color = "#000";
+    } else if (e.target.scrollTop < 3 * window.innerHeight) {
+      document.getElementById("issue").style.backgroundColor = "#00C1B5";
+      document.body.style.color = "#000";
+    } else if (e.target.scrollTop < 4 * window.innerHeight) {
+      document.getElementById("issue").style.backgroundColor = "#FF6519";
+      document.body.style.color = "#000";
+    } else if (e.target.scrollTop < 5 * window.innerHeight) {
+      document.getElementById("issue").style.backgroundColor = "#FFBE00";
+      document.body.style.color = "#000";
+    } else if (e.target.scrollTop < 6 * window.innerHeight) {
+      document.getElementById("issue").style.backgroundColor = "#1D3FBB";
+      document.body.style.color = "#000";
+    } else if (e.target.scrollTop < 7 * window.innerHeight) {
+      document.getElementById("issue").style.backgroundColor = "#E30512";
+      document.body.style.color = "#000";
+    }
+  };
+  useEffect(() => {
+    console.log(scrolly);
+    minScrollHandler();
+  }, [scrollY]);
   return (
-    <div className="flex flex-col lg:snap-y lg:snap-mandatory lg:h-screen w-full  overflow-y-scroll ">
+    <div
+      className="flex flex-col lg:snap-y lg:snap-mandatory lg:h-screen w-full  overflow-y-scroll "
+      onScroll={scrollHandler}
+    >
       <section
         className="fixed top-0 w-screen h-screen -z-30 right-0 bg-white text-black"
         id="issue"
